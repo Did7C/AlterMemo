217ff286180ab9ed72f3e54eadf49437a22f47c6
Echeverria-Didier <didbidean@hotmail.com>
2024-02-01 16:52:08 +0100

    backend/database/schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,49 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+DROP TABLE IF EXISTS Utilisateur;
+
+DROP TABLE IF EXISTS Tache;
+
+DROP TABLE IF EXISTS ListeDeTaches;
+
+DROP TABLE IF EXISTS TacheDansListe;
+
+DROP TABLE IF EXISTS TableDejointure;
+
+
+CREATE TABLE Utilisateur
+(
+  idUtilisateur int PRIMARY KEY,
+  nomUtilisateur varchar
+(255)
 );
+
+CREATE TABLE Tache
+(
+  idTache int PRIMARY KEY,
+  description varchar
+(255),
+  estTerminee bool,
+  dateEcheance date,
+  idUtilisateur int
+);
+
+CREATE TABLE ListeDeTaches
+(
+  idListe int PRIMARY KEY,
+  nomListe varchar
+(255)
+);
+
+CREATE TABLE TacheDansListe
+(
+  idTacheDansListe int PRIMARY KEY,
+  idListe int,
+  idTache int
+);
+
+CREATE TABLE TableDeJointure
+(
+  id int PRIMARY KEY,
+  idUtilisateur int,
+  idListe int
+);
+
